892381e547b7f0f80b344d9a216f0e836a6258a2
Gayatri Naiknware <gayatrinaiknware@gmail.com>
2026-06-05 21:06:00 +0530

    fix: dynamic port

--- career-backend/server.js
+++ career-backend/server.js
@@ -68,4 +68,5 @@ app.post("/login", async (req, res) => {
     }
 });
 
-app.listen(5000, () => console.log("🚀 Server running on 5000"));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
